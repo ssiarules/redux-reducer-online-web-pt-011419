89a12e1386cd582d1963c0d4854154d443dfc1ea
Johnson Joseph <jhonsonj88@gmail.com>
2019-10-29 00:01:29 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,16 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case "ADD_FRIEND":
+      friend: {
+        name: " "
+        hometown: " ",
+        id: 1
+        return {friends: [ ]}
+      
+      }
+
+
+    default:
+      return state;
+  }
 }
